1263ebaaa4f6fb098451d90043bf80f04f16a3b3
The-greenpanter <judpenaca@unal.edu.co>
2024-07-10 08:53:10 -0500

    BoostrapCards done and button edit done, delete button missing

---
+++ src/components/Person.tsx
@@ -0,0 +1,83 @@
+// import PropTypes from 'prop-types';
+
+// export const Person = ({ name = '', role = '', img = '' }) => {
+
+//     return (
+//         <div className='col'>
+//             <div className='card' style={{ width: "18rem" }}>
+//                 <img className='card-img-top' src={img} alt={name} />
+//                 <div className='card-body'>
+//                     <h4 className='card-title'>{name}</h4>
+//                     <p className='card-text'>{role}</p>
+//                 </div>
+//                 <div className='container mb-4 text-center'>
+//                     <button className='btn btn-success me-2'>Editar</button>
+//                     <button className='btn btn-danger'>Eliminar</button>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+
+// Person.propTypes = {
+//     name: PropTypes.string.isRequired,
+//     role: PropTypes.string.isRequired,
+//     img: PropTypes.string.isRequired
+// }
+import PropTypes from 'prop-types';
+
+export const Person = ({ name, role, img, handleEdit }) => {
+
+    return (
+        <div className='col'>
+            <div className='card' style={{ width: "18rem" }}>
+                <img className='card-img-top' src={img} alt={name} />
+                <div className='card-body'>
+                    <h4 className='card-title'>{name}</h4>
+                    <p className='card-text'>{role}</p>
+                </div>
+                <div className='container mb-4 text-center'>
+                    <button className='btn btn-success me-2' onClick={handleEdit}>Editar</button>
+                    <button className='btn btn-danger'>Eliminar</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+Person.propTypes = {
+    name: PropTypes.string.isRequired,
+    role: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    handleEdit: PropTypes.func
+}
+// import PropTypes from 'prop-types';
+
+// export const Person = ({ name="", role="", img="", handleEdit =()=> {} }) => {
+
+//     return (
+//         <div className='col'>
+//             <div className='card' style={{ width: "18rem" }}>
+//                 <img className='card-img-top' src={img} alt={name} />
+//                 <div className='card-body'>
+//                     <h4 className='card-title'>{name}</h4>
+//                     <p className='card-text'>{role}</p>
+//                 </div>
+//                 <div className='container mb-4 text-center'>
+//                     <button className='btn btn-success me-2' onClick={handleEdit}>Editar</button>
+//                     <button className='btn btn-danger'>Eliminar</button>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+
+// Person.propTypes = {
+//     name: PropTypes.string.isRequired,
+//     role: PropTypes.string.isRequired,
+//     img: PropTypes.string.isRequired,
+//     handleEdit: PropTypes.func
+// }
